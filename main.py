22445e607669f2de83b177a6aa9ff78ebec0a162
Aaditya Tile <adi01tile@gmail.com>
2021-03-20 16:44:35 +0530

    context stuff done, works better

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pickle
 import json
 import random
 import pyttsx3
+from colorama import init, Fore
 from utils import clean_pattern, define_network, bag_of_words
 
 with open('saved_variables.pickle', 'rb') as file:
@@ -11,10 +12,10 @@ with open('saved_variables.pickle', 'rb') as file:
 with open('intents.json') as file:
     data = json.load(file)
 
+init(autoreset=True)
 engine = pyttsx3.init()
 model = define_network(X, y)
 model.load("chatbot_model.tflearn")
-
 
 # to handle previous context and give advantage to results of that context
 def context_func(context, user_input):
@@ -29,7 +30,8 @@ def context_func(context, user_input):
 						results[tg_index] += 0.5 
 	return results
 
-def chat():
+
+def chat(show_tags_probability= False):
 	probability_threshold = 0.4 
 	context = ""
 	print("Welcome to the intents based chatbot. You can start chatting! (enter 'q' to quit)")
@@ -44,8 +46,13 @@ def chat():
 		else:
 			model_input = [bag_of_words(user_input, stemmed_words, ignore_words)] #as model is trained on 2d array
 			results = model.predict(model_input)[0] #gives array of probabilities for all tags
-		for i, j in zip(tags, results):
-			print(i, j)
+		# to show probabilities given by model for each tags
+		if show_tags_probability:
+			probability_dict = {}
+			for i, j in zip(tags, results):
+				probability_dict[i] = j
+			print(Fore.GREEN + 'tags_probabilities: {}'.format(probability_dict))
+			probability_dict.clear()
 		context = "" #reset the context
 		result_index = np.argmax(results) #to get index of max probability
 		#to filter out predictions below threshold
@@ -64,6 +71,4 @@ def chat():
 		engine.say(response)
 		engine.runAndWait()
 
-chat()
-
-# TODO: retrain and test main.py
+chat(True)
